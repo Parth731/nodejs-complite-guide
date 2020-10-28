58c70073a5d26a405f39bfd5a3de8f4f935916da
Parth731 <patelparth31795@gmail.com>
2020-10-28 17:40:42 +0530

    javascript  first project

---
+++ javascript refresher/intro.js
@@ -0,0 +1,13 @@
+/*
+
+java script is 
+
+-weakly typed language
+-object oriended langauage
+-versatile langauage
+-no explicit type assignment and datatype canbe dynamically
+-data canbe organized in logical objects(primitive and refrence type)
+-runs in browser and directly on pc/server(can perfrom a broad variely of tasks)
+
+
+*/
